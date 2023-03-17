1c3da3ab0c7b3c9bdb496205e95428d8de4a663a
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-17 21:05:45 +0100

    Add adding 'active' class to the clicked link

--- js/script.js
+++ js/script.js
@@ -20,18 +20,20 @@ po kliknięciu linka:
 }); */
 
 const titleClickHandler = function (event) {
+  const clickedElement = this;
   console.log('Link was clicked');
-  console.log('event', event);
 
-  /* remove class 'active' from all article links */
+  /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
   for (let activeArticle of activeArticles) {
