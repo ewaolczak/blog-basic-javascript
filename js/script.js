85e09a529f363408d7fc2faae76391a30d176136
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-17 20:47:51 +0100

    Add titleClickHandler for articles links and add script algorithm in comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+/*
+po kliknięciu linka:
+  1 ustaw klasy linków:
+    - usuń klasę active ze wszystkich linków na liście tytułów,
+    - dodaj klasę active do klikniętego linka,
+  2 ukryj wszystkie artykuły:
+    - usuń klasę active ze wszystkich artykułów,
+  3 znajdź artykuł do wyświetlenia:
+    - z klikniętego linka weź zawartość atrybutu href, np. #article-2,
+    - znajdź na stronie element pasujący do selektora takiego, jak wartość atrybutu href, np. #article-2 – czyli szukamy elementu o id="article-2",
+  4 wyświetl znaleziony artykuł:
+    - dodaj klasę active do znalezionego artykułu.
+*/
+
+/* document.getElementById('test-button').addEventListener('click', function () {
+  const links = document.querySelectorAll('.titles a');
+  console.log('links', links);
+}); */
+
+const titleClickHandler = function (event) {
+  console.log('Link was clicked');
+  console.log('event', event)
+
+  /* remove class 'active' from all article links */
+  /* add class 'active' to the clicked link */
+  /* remove class 'active' from all articles */
+  /* get 'href' attribute from the clicled link */
+  /* find the correct article using the selector (value of 'href' attribute) */
+  /* add class 'active' tp the correct article */
+};
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
